1b6ff6a72c42ea95d59117bd1fc5bbef0cd92c1e
Mukhlas112 <Mukhlaspes@gmail.com>
2025-12-05 19:52:00 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// --- index.js ---
-require('dotenv').config(); // Load variabel dari .env
-const app = require('./src/app'); // Import konfigurasi app dari folder src
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
